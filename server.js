e27e33d1015505e2822d8e9825c19941d8d9b151
niicoodavid-create <niicoodavid@gmail.com>
2026-09-22 18:23:52 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ const ADMIN_EMAIL = 'gamarramartin1995@gmail.com';
 const DATA_FILE = path.join(__dirname, 'data.json');
 let db = {
   servicios: [
-    { id: 1, nombre: 'Corte clásico', precio: 15000 },
-    { id: 2, nombre: 'Corte + barba', precio: 18000 },
-    { id: 3, nombre: 'global', precio: 60000 },
-    { id: 4, nombre: 'mechas ', precio: 55000 },
-    { id: 5, nombre: 'Corte premium', precio: 20000 }
+    { id: 1, nombre: 'Corte clásico/fade', precio: '$14,000' },
+    { id: 2, nombre: 'corte premium(barba/ceja/limpieza facial)', precio: '$18,000' },
+    { id: 3, nombre: 'corte/barba', precio: '$18,000' },
+    { id: 4, nombre: 'color global+corte(4 a 5hs aprox)', precio: '$60,000' },
+    { id: 5, nombre: 'mechas+corte', precio: '$50,000' }
   ],
   clientes: [
     { id: 1, nombre: 'Martin Admin', email: ADMIN_EMAIL, rol: 'barbero' }
